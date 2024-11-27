8bd571e1e924dc354cef1ce416416d35948a9874
Aryan Pahari <76030680+Aryan-coder-student@users.noreply.github.com>
2024-11-27 14:19:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ class ProjectIdeaGenerator:
             - Preferred technological approach
             
             ## Output: 
-            - output in proper heading , subheading ,points , like project title should be in proper haeding , in Italic and underline 
+            - output in proper heading , subheading ,points , like project title should be in proper haeding , in Italic and differetn font 
             - make sub heading in indentations 
             - ouput in only in markdown 
             - Add A divider after every project
